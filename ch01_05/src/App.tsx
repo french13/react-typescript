ea23297755ba5dc18549d0442292aae1d97ce2cf
DESKTOP-6VNKTFM\user <junghun6@gmail.com>
2023-06-09 18:06:55 +0900

    리액트 프로젝트

--- ch01_05/src/App.tsx
+++ ch01_05/src/App.tsx
@@ -2,12 +2,29 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import * as D from './data'
+import type {DragEvent} from 'react'
 
-const App = () => {
+const App = (e: any) => {
+  const onDragStart = (e: DragEvent<HTMLElement>) =>
+    console.log('onDragStart', e.dataTransfer)
+  const onDragEnd = (e: DragEvent<HTMLElement>) =>
+    console.log('onDragEnd', e.dataTransfer)
+
+  const onDragOver = (e: DragEvent) => e.preventDefault()
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    console.log('onDrop', e.dataTransfer)
+  }
+
   return (
     <div className="App">
-      <p>{D.randomName()}</p>
-      <img src={D.randomAvatar()} height="50" />
+      <p>DragDrop</p>
+      <div draggable onDragStart={onDragStart} onDragEnter={onDragEnd}>
+        <h1>Drag Me</h1>
+      </div>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop over Me</h1>
+      </div>
     </div>
   )
 }
