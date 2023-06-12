83c8d9a2ba0bb5a4919c6b59a173fee16b45c1b0
DESKTOP-TVRU0MM\Admin <skswnddl6@gmail.com>
2023-06-12 21:03:54 +0900

    setting

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,5 +1,26 @@
+import {useState} from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import UseRef from './components/UseRef'
+
+const menuList = [{title: 'useRef', link: '/useRef'}]
+
 const App = () => {
-  return <div className="App">테스트</div>
+  const [menu, setMenu] = useState(menuList)
+
+  return (
+    <div className="App">
+      <div>
+        {menu.map((item, index) => (
+          <button>
+            <Link to={item.link}>{item.title}</Link>
+          </button>
+        ))}
+      </div>
+      <Routes>
+        <Route path="/useRef" element={<UseRef />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ study/src/components/UseRef.tsx
@@ -0,0 +1,5 @@
+const UseRef = () => {
+  return <div>useRef</div>
+}
+
+export default UseRef
