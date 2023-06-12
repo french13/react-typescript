a0ed1116588e6f4c1861b1c2da15380bc6a0ac12
DESKTOP-TVRU0MM\Admin <skswnddl6@gmail.com>
2023-06-12 20:37:03 +0900

    study

---
+++ study/.prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bracketSpacing: false,
+  // 객체 리터럴의 중괄호 주위에 공백을 추가
+  jsxBrachetSameLine: true,
+  //닫는 괄호를 같은 줄에 위치
+  singleQuote: true,
+  // 문자열의 따옴표를 작은 따옴표로 설정
+  trailingComma: 'none',
+  // none으로 설정시 마지막 항목 뒤에 쉼표를 추가하지 않음
+  arrowParens: 'avoid',
+  // 화살표 함수의 파라미터가 1개일 경우 괄호를 생략
+  semi: false,
+  printWidth: 90
+  // 한줄의 최대 길이
+}
