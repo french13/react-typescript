c0358231f891130f7342ad3d96d9cf5accdc4bfc
DESKTOP-TVRU0MM\Admin <skswnddl6@gmail.com>
2023-06-13 21:35:27 +0900

    useImperativeHandle

--- study/src/components/UseImperativeHandle.tsx
+++ study/src/components/UseImperativeHandle.tsx
@@ -1,7 +1,46 @@
+import {forwardRef, useRef, useEffect, useImperativeHandle} from 'react'
+
+//parentComponents
+const ParentComponents = () => {
+  const childRef = useRef<any>(null)
+
+  const clickParentButton = () => {
+    console.log(childRef.current?.onAlert())
+  }
+
+  return (
+    <div>
+      <div>
+        부모컴포넌트
+        <button onClick={clickParentButton}>부모</button>
+        <ChildComponents2 ref={childRef} />
+      </div>
+    </div>
+  )
+}
+
+//childcomponents
+const ChildComponents = (props: any, ref: any) => {
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        onAlert: () => alert('자식컴포넌트')
+      }
+    },
+    []
+  )
+
+  return <div>자식컴포넌트</div>
+}
+
+const ChildComponents2 = forwardRef(ChildComponents)
+
 const UseImperativeHandle = () => {
   return (
     <div>
       <p>UseImperativeHandle 예제</p>
+      <ParentComponents />
     </div>
   )
 }
